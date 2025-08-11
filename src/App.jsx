f4464d208f06e170cc49b95a3b340809758e7f6b
Biloliddin-Tursunov <tursunovbiloliddin2006@gmail.com>
2025-08-11 14:49:01 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 /** @format */
 
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Memories from "./pages/Memories";
 import Schedule from "./pages/Schedules";
+import Memories from "./pages/Memories";
 
 export default function App() {
   return (
     <div className="p-5">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/schedules" element={<Schedule />} />
+        <Route path="/" element={<Schedule />} />
         <Route path="/memories" element={<Memories />} />
       </Routes>
     </div>
